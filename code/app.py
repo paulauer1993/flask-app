45319a82ee9bf562bcc644cd6ecadb7c444536bd
unknown <auer_paul@yahoo.com>
2019-06-04 02:33:36 +0300

    html

--- code/app.py
+++ code/app.py
@@ -36,7 +36,21 @@ api.add_resource(Light, "/api/light/<int:uid>")
 api.add_resource(UserRegister, "/api/register")
 api.add_resource(UserLogin, "/api/login")
 
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
 
+@app.route("/login")
+def form_post():
+    return render_template("login.html")
+    # if request.method == "POST":
+    #     username = request.form["username"]
+    #     password = request.form["password"]
+    #     r = requests.post("https://senzori.herokuapp.com/api/login", data={"username": username, "password": password}, headers={"Content-Type": "application/json"})
+    #     if r.status_code == "200":
+    #         return render_template("#"), 200
+    #     else:
+    #         return render_template("login.html"), 404
 
 
 if __name__ == "__main__":
@@ -44,20 +58,3 @@ if __name__ == "__main__":
     db.init_app(app)
     app.run(port=5000, debug=True)
 
-
-    @app.route("/")
-    def index():
-        return redirect(url_for("login"))
-
-
-    @app.route("/login")
-    def form_post():
-        return render_template("login.html")
-        # if request.method == "POST":
-        #     username = request.form["username"]
-        #     password = request.form["password"]
-        #     r = requests.post("https://senzori.herokuapp.com/api/login", data={"username": username, "password": password}, headers={"Content-Type": "application/json"})
-        #     if r.status_code == "200":
-        #         return render_template("#"), 200
-        #     else:
-        #         return render_template("login.html"), 404
